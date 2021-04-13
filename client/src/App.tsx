598d6218e3349b2a2fabeb92e5e182171586e789
arasto <Arasto.Sahbaei@gmail.com>
2021-04-13 10:50:54 +0200

    can now create qr codes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import QRCode from "react-qr-code";
+
 export const App = () => {
 	return (
 		<div>
 			<h1>Queue System</h1>
+			<QRCode value="klicka på länken för att få en köplapp till systembolaget: https://www.youtube.com" />
 		</div>
 	)
 }
